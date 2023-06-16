229894cc40c5213849e195f84981a8e1435fdf74
Abigail Amani Wanjiku <abigail.wanjiku@student.moringaschool.com>
2023-06-16 08:40:08 +0300

    arrow functions complete

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide(num1, num2) {
+    num1 = 2000
+    num2 = 100
+    return num1 / num2
+}
+divide()
+
+const square = (parameter) => parameter * parameter
+square(2)
+
+const add = (par1, par2) => par1 + par2
+add(2, 3)
